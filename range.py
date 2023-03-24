d5b9f75e4d0a32a12de757b2810a50a6ff8a23b4
Cvaaaa <118925407+Cvaaaa@users.noreply.github.com>
2023-03-24 17:13:36 +0530

    Create range.py

---
+++ range.py
@@ -0,0 +1,11 @@
+for num in range(6):
+    print("num=",num)
+print("*****************")
+for num in range(10,16):
+    print("num=",num)
+print("*****************")
+for num in range(40,60,3):
+    print("num=",num)
+print("*****************")
+for num in range(100,20,-10):
+    print("num=",num)
