9347b37a73ac2e02fea9d927e7cdd92bded887af
Cvaaaa <118925407+Cvaaaa@users.noreply.github.com>
2023-05-15 22:17:35 +0530

    Create even.py

---
+++ even.py
@@ -0,0 +1,8 @@
+for even in range(70,80,2):
+    print(even)
+print("######################")
+for odd in range(61,70,2):
+    print(odd)
+print("######################")
+for mul in range(20,50,5):
+    print(mul)
